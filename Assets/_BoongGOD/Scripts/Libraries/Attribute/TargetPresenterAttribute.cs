bef8772f93ec2bd2547080c05f9e091bc1bccb44
Dinn <dinn@richalien.net>
2024-04-26 15:03:59 +0900

    add unity editor symbol

--- Assets/_BoongGOD/Scripts/Libraries/Attribute/TargetPresenterAttribute.cs
+++ Assets/_BoongGOD/Scripts/Libraries/Attribute/TargetPresenterAttribute.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using UnityEditor;
 using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace Redbean.Static
 {
