a7d8290fe2073a043d2154945d64fb27b12dbcd8
Dinn <dinn@richalien.net>
2024-07-03 16:58:53 +0900

    API 컨트롤러 이름 변경

--- Assets/_/Scripts/Contents/Common/Api/Request/ApiGetRequest.cs
+++ Assets/_/Scripts/Contents/Common/Api/Request/ApiGetRequest.cs
@@ -5,18 +5,18 @@ namespace Redbean.Api
 	public class ApiGetRequest : ApiBase
 	{
 		public static async Task<ApiResponse<UserAndTokenResponse>> GetAccessTokenAndUserRequest(params object[] args) =>
-			await SendGetRequest<ApiResponse<UserAndTokenResponse>>("/Authentication/GetAccessTokenAndUser?id={0}", args);
+			await SendGetRequest<ApiResponse<UserAndTokenResponse>>("/GetAuthorize/GetAccessTokenAndUser?id={0}", args);
 
 		public static async Task<ApiResponse<TokenResponse>> GetAccessTokenRefreshRequest(params object[] args) =>
-			await SendGetRequest<ApiResponse<TokenResponse>>("/Authentication/GetAccessTokenRefresh?token={0}", args);
+			await SendGetRequest<ApiResponse<TokenResponse>>("/GetAuthorize/GetAccessTokenRefresh?token={0}", args);
 
 		public static async Task<ApiResponse<AppConfigResponse>> GetAppConfigRequest(params object[] args) =>
-			await SendGetRequest<ApiResponse<AppConfigResponse>>("/Common/GetAppConfig", args);
+			await SendGetRequest<ApiResponse<AppConfigResponse>>("/GetSetting/GetAppConfig", args);
 
 		public static async Task<ApiResponse<TableConfigResponse>> GetTableConfigRequest(params object[] args) =>
-			await SendGetRequest<ApiResponse<TableConfigResponse>>("/Common/GetTableConfig", args);
+			await SendGetRequest<ApiResponse<TableConfigResponse>>("/GetSetting/GetTableConfig", args);
 
 		public static async Task<ApiResponse<TableResponse>> GetTableRequest(params object[] args) =>
-			await SendGetRequest<ApiResponse<TableResponse>>("/Common/GetTable", args);
+			await SendGetRequest<ApiResponse<TableResponse>>("/GetSetting/GetTable", args);
 	}
 }
--- Assets/_/Scripts/Contents/Common/Api/Request/ApiPostRequest.cs
+++ Assets/_/Scripts/Contents/Common/Api/Request/ApiPostRequest.cs
@@ -5,19 +5,19 @@ namespace Redbean.Api
 	public class ApiPostRequest : ApiBase
 	{
 		public static async Task<ApiResponse<StringResponse>> PostEditorAccessTokenRequest(StringRequest args) =>
-			await SendPostRequest<ApiResponse<StringResponse>>("/Authentication/PostEditorAccessToken", args);
+			await SendPostRequest<ApiResponse<StringResponse>>("/EditAccess/PostEditorAccessToken", args);
 
 		public static async Task<ApiResponse<AppVersionResponse>> PostAppVersionRequest(AppVersionRequest args) =>
-			await SendPostRequest<ApiResponse<AppVersionResponse>>("/Config/PostAppVersion", args);
+			await SendPostRequest<ApiResponse<AppVersionResponse>>("/EditConfig/PostAppVersion", args);
 
 		public static async Task<ApiResponse<AppConfigResponse>> PostAppMaintenanceRequest(AppMaintenanceRequest args) =>
-			await SendPostRequest<ApiResponse<AppConfigResponse>>("/Config/PostAppMaintenance", args);
+			await SendPostRequest<ApiResponse<AppConfigResponse>>("/EditConfig/PostAppMaintenance", args);
 
 		public static async Task<ApiResponse<StringArrayResponse>> PostTableFilesRequest(AppUploadFilesRequest args) =>
-			await SendPostRequest<ApiResponse<StringArrayResponse>>("/Storage/PostTableFiles", args);
+			await SendPostRequest<ApiResponse<StringArrayResponse>>("/EditFiles/PostTableFiles", args);
 
 		public static async Task<ApiResponse<StringArrayResponse>> PostBundleFilesRequest(AppUploadFilesRequest args) =>
-			await SendPostRequest<ApiResponse<StringArrayResponse>>("/Storage/PostBundleFiles", args);
+			await SendPostRequest<ApiResponse<StringArrayResponse>>("/EditFiles/PostBundleFiles", args);
 
 		public static async Task<ApiResponse> PostUserNicknameRequest(StringRequest args) =>
 			await SendPostRequest<ApiResponse>("/User/PostUserNickname", args);
